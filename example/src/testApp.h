ee5c71e0fec0be56ebe649909ce2520e2ca4619d
Kyle McDonald <kyle@kylemcdonald.net>
2012-08-06 20:56:22 -0400

    added naive point export, renamed project, timing sensor (49.8fps)

--- example/src/testApp.h
+++ example/src/testApp.h
@@ -4,6 +4,7 @@
 #include "LMS1xx.h"
 //#include "ofxKmeans.h"
 #include "ofxOpenCv.h"
+#include "RateTimer.h"
 
 #define NUM_SAMPLES 541
 #define NUM_TIME_SAMPLES 1
@@ -18,87 +19,92 @@
 
 
 class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-        
-        
-        void drawRawData();
-        void drawPolar2CartData();
-        void polar2CartData();
-        void findClusterCentroids();
-        void drawTrackingAndRawData();
-        void filterBlobs();
-        void drawFilteredBlobs();
-        void filterSingleBlobs();
-        void drawSingleFilteredBlob();
-    
-        vector<int> clusterPoints(vector<ofPoint> data, unsigned nClusters);
-    
-        LMS1xx laser;
-        scanData dataBuf;
-        
-        uint16_t distVals[541];
-        
-        //Ring buffer of values
-        uint16_t bufferDistVals[NUM_TIME_SAMPLES][541];
-        
-        //Current buffer in ring
-        int currentSample;
-    
-        vector<ofPoint> trackedPts;
-        ofPoint allPts[541];
-        
-        //centroids of clusters
-        vector<ofPoint> centroids;
-        
-        ofPoint blob1Prev;
-    
-        ofPoint blob2Prev;
-    
-        ofPoint blob1Avg;
-    
-        ofPoint blob2Avg;
-        
-        ofPoint blobRingBuffer[NUM_BLOB_SAMPLES];
-        int currentBlobRingSample;
-        bool blobVisible;
-        bool runningBlobOpacityRampUp;
-        bool runningBlobOpacityRampDown;
-        float blobOpacity;
-        ofPoint filterdSingleBlob;
-        
-    
-    
-        double avgDistVals[541];
-    
-        bool scanning;
-    
-        ofImage softcircle;
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	
+	void drawRawData();
+	void drawPolar2CartData();
+	void polar2CartData();
+	void findClusterCentroids();
+	void drawTrackingAndRawData();
+	void filterBlobs();
+	void drawFilteredBlobs();
+	void filterSingleBlobs();
+	void drawSingleFilteredBlob();
+	
+	vector<int> clusterPoints(vector<ofPoint> data, unsigned nClusters);
+	
+	LMS1xx laser;
+	scanData dataBuf;
+	
+	vector<scanData> allData;
+	
+	uint16_t distVals[541];
+	
+	//Ring buffer of values
+	uint16_t bufferDistVals[NUM_TIME_SAMPLES][541];
+	
+	//Current buffer in ring
+	int currentSample;
+	
+	vector<ofPoint> trackedPts;
+	ofPoint allPts[541];
+	
+	//centroids of clusters
+	vector<ofPoint> centroids;
+	
+	ofPoint blob1Prev;
+	
+	ofPoint blob2Prev;
+	
+	ofPoint blob1Avg;
+	
+	ofPoint blob2Avg;
+	
+	ofPoint blobRingBuffer[NUM_BLOB_SAMPLES];
+	int currentBlobRingSample;
+	bool blobVisible;
+	bool runningBlobOpacityRampUp;
+	bool runningBlobOpacityRampDown;
+	float blobOpacity;
+	ofPoint filterdSingleBlob;
+	
+	
+	
+	double avgDistVals[541];
+	
+	bool scanning;
+	
+	ofImage softcircle;
+	
+	RateTimer timer;
 };
 
 typedef struct _clusterInfo {
-    int numPoints;
-    
-    double xSum;
-    
-    double ySum;
-    
-    double xCentroid;
-    
-    double yCentroid;
-    
+	int numPoints;
+	
+	double xSum;
+	
+	double ySum;
+	
+	double xCentroid;
+	
+	double yCentroid;
+	
 }clusterInfo;
 
