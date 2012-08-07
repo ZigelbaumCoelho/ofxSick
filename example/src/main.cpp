028545ed2b022c74b03e709843a2222a5c25a6e6
Kyle McDonald <kyle@kylemcdonald.net>
2012-08-07 15:11:44 -0400

    drawing markers to show range and forward direction

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -2,15 +2,8 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
-
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
-
+int main() {
+	ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 800, 800, OF_WINDOW);
+	ofRunApp(new testApp());
 }
